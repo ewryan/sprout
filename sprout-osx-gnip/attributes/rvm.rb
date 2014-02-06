2218bbcffb6524ac9ce54787e080fde8ebe7541f
Eric Ryan <you@example.com>
2014-02-06 15:11:55 -0700

    er: change ruby versions that are install by default

--- sprout-osx-gnip/attributes/rvm.rb
+++ sprout-osx-gnip/attributes/rvm.rb
@@ -2,9 +2,8 @@ node.default['versions']['rvm'] = '18048d56515699dc8f97f1a85f36526b87b4a721'
 
 node.default["rvm"] ||= {}
 node.default["rvm"]["rubies"] = {
-    "ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
-    "ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
-    "ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" },
+    #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
+    #"ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" },
     "ruby-2.0.0-p353" => { :command_line_options => "--verify-downloads 1" }
 }
 
