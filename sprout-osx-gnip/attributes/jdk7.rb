ef03ec612c7d1a135347f0ea7e9a27245ddd050f
Eric Ryan <you@example.com>
2014-02-06 14:27:20 -0700

    er: add rvm recipe

--- sprout-osx-gnip/attributes/jdk7.rb
+++
@@ -1,3 +0,0 @@
-default['sprout']['jdk7']['dmg']['source']      = "http://gnipit.s3.amazonaws.com/sprout/jdk-7u51-macosx-x64.dmg"
-default['sprout']['jdk7']['dmg']['volumes_dir'] = "JDK\ 7\ Update\ 51"
-default['sprout']['jdk7']['dmg']['checksum']    = "1312b0553bd37a232114637f05b036a6fd5d444e653adef7230ba141328f7734"
